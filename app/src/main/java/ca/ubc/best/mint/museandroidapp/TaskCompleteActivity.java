8530116c78ecb6ace42468a77c6c904681d9fd90
padster <padsterpat@gmail.com>
2018-02-04 18:19:08 -0800

    Average ERP shown in red, and normalization removed

--- app/src/main/java/ca/ubc/best/mint/museandroidapp/TaskCompleteActivity.java
+++ app/src/main/java/ca/ubc/best/mint/museandroidapp/TaskCompleteActivity.java
@@ -102,12 +102,19 @@ public class TaskCompleteActivity extends AppCompatActivity {
   /// Some features need to be added to the epoch viewer, then this can be removed.
   ///
   private static List<Map<String, TimeSeriesSnapshot<Double>>> hackNormalize(
-      List<Map<String, TimeSeriesSnapshot<Double>>> epoch, int zeroSample) {
+      List<Map<String, TimeSeriesSnapshot<Double>>> epochs, int zeroSample) {
+    int shortestEpochLength = Integer.MAX_VALUE;
+    for (int i = 0; i < epochs.size(); i++) {
+      for (TimeSeriesSnapshot<Double> epoch : epochs.get(i).values()) {
+        shortestEpochLength = Math.min(shortestEpochLength, epoch.length);
+      }
+    }
+
     double minDelta = 0.0;
     double maxDelta = 0.0;
-    for (int i = 0; i < epoch.size(); i++) {
-      for (String key : epoch.get(i).keySet()) {
-        Double[] values = epoch.get(i).get(key).values;
+    for (int i = 0; i < epochs.size(); i++) {
+      for (String key : epochs.get(i).keySet()) {
+        Double[] values = epochs.get(i).get(key).values;
         for (int j = 0; j < values.length; j++) {
           double delta = values[j] - values[zeroSample];
           minDelta = Math.min(delta, minDelta);
@@ -116,25 +123,28 @@ public class TaskCompleteActivity extends AppCompatActivity {
       }
     }
     List<Map<String, TimeSeriesSnapshot<Double>>> result = new ArrayList<>();
-    for (int i = 0; i < epoch.size(); i++) {
+    for (int i = 0; i < epochs.size(); i++) {
       Map<String, TimeSeriesSnapshot<Double>> normed = new HashMap<>();
-      for (String key : epoch.get(i).keySet()) {
-        normed.put(key, hackNormalize(epoch.get(i).get(key), zeroSample, minDelta, maxDelta));
+      for (String key : epochs.get(i).keySet()) {
+        normed.put(key, hackNormalize(epochs.get(i).get(key),
+            shortestEpochLength, zeroSample, minDelta, maxDelta));
       }
       result.add(normed);
     }
     return result;
   }
   private static TimeSeriesSnapshot<Double> hackNormalize(
-      TimeSeriesSnapshot<Double> snapshot, int zeroSample, double minDelta, double maxDelta) {
-    int n = snapshot.values.length;
-    Double[] normValues = new Double[n];
-    for (int i = 0; i < n; i++) {
+      TimeSeriesSnapshot<Double> snapshot,
+      int length, int zeroSample, double minDelta, double maxDelta
+  ) {
+    long[] normTimestamps = new long[length];
+    Double[] normValues = new Double[length];
+    for (int i = 0; i < length; i++) {
+      normTimestamps[i] = snapshot.timestamps[i];
       double delta = snapshot.values[i] - snapshot.values[zeroSample];
       normValues[i] = (delta - minDelta) / (maxDelta - minDelta);
-      normValues[i] = Constants.VOLTAGE_MIN + normValues[i] * (Constants.VOLTAGE_MAX - Constants.VOLTAGE_MIN);
     }
-    return new TimeSeriesSnapshot<Double>(snapshot.timestamps, normValues);
+    return new TimeSeriesSnapshot<>(normTimestamps, normValues);
   }
   ///
   /// End hack :)
