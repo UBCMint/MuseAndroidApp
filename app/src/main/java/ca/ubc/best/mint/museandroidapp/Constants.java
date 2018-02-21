eac027992afb69064b721adec229c0ea2579dd49
padster <padsterpat@gmail.com>
2018-02-20 22:22:25 -0800

    Add a way to store historic results, update the store on each run, display the past and let a user look into them later

--- app/src/main/java/ca/ubc/best/mint/museandroidapp/Constants.java
+++ app/src/main/java/ca/ubc/best/mint/museandroidapp/Constants.java
@@ -1,9 +1,17 @@
 package ca.ubc.best.mint.museandroidapp;
 
+import java.text.SimpleDateFormat;
+
 public class Constants {
   /**
    * Muse send frequency data back at this rate:
    * http://developer.choosemuse.com/tools/available-data#Absolute_Band_Powers
    */
   public static final int FREQUENCY_HZ = 10;
+
+  /** Location of file for historic results. */
+  public static final String HISTORIC_RESULTS_FILE = "historicResults.dat";
+
+  /** How to format dates to string. */
+  public static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("MMM d, yyyy");
 }
---
+++ app/src/main/java/ca/ubc/best/mint/museandroidapp/ResultListActivity.java
@@ -0,0 +1,30 @@
+package ca.ubc.best.mint.museandroidapp;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import ca.ubc.best.mint.museandroidapp.analysis.HistoricResults;
+import ca.ubc.best.mint.museandroidapp.databinding.ActivityResultListBinding;
+
+public class ResultListActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    ActivityResultListBinding binding =
+        DataBindingUtil.setContentView(this, R.layout.activity_result_list);
+    // TODO: add stuff to binding? Currently nothing is used in activity_result_list.xml
+
+    HistoricResults results = HistoricResults.loadFromFile(this);
+
+    // Hook up the binding for the list of devices.
+    RecyclerView listView = (RecyclerView) findViewById(R.id.resultList);
+    listView.setLayoutManager(new LinearLayoutManager(this));
+    listView.setAdapter(new ResultListAdapter(this, results));
+  }
+
+}
